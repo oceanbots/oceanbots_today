6675ab7a6a4d2214bdfe034ac9b40eefad40c244
oceanbots <oceanbots@gmail.com>
2016-11-24 20:33:38 -0500

    WIP reorganizing cutom meta box code

--- library/post-ship.php
+++ library/post-ship.php
@@ -217,6 +217,65 @@ function ship_save_meta_box_data( $post_id ) {
 add_action( 'save_post', 'ship_save_meta_box_data' );
 	
 
+/**
+ * When the post is saved, saves our custom data.
+ *
+ * @param int $post_id The ID of the post being saved.
+ *
+ * I don't think there's any reason to keep this as one function, 
+ * instead of having a different one for each box
+ */
+function ship_save_meta_box_data( $item_name, $nonce_name, $post_id ) {
+
+	/*
+	 * We need to verify this came from our screen and with proper authorization,
+	 * because the save_post action can be triggered at other times.
+	 */
+
+	// Check if our nonces are set.
+	if ( ! isset( $_POST[$nonce_name] ) ) {
+		return;
+	}
+
+	// Verify that the nonce is valid.
+	if ( ! wp_verify_nonce( $_POST[$nonce_name], 'ship_save_meta_box_data' ) ) {
+		return;
+	}
+
+	// If this is an autosave, our form has not been submitted, so we don't want to do anything.
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
+
+	// Check the user's permissions.
+	if ( isset( $_POST['post_type'] ) && 'page' == $_POST['post_type'] ) {
+
+		if ( ! current_user_can( 'edit_page', $post_id ) ) {
+			return;
+		}
+
+	} else {
+
+		if ( ! current_user_can( 'edit_post', $post_id ) ) {
+			return;
+		}
+	}
+
+	// WE ARE HERE
+
+	/* OK, it's safe for us to save the data now. */
+	
+	// Make sure that it is set.
+	if ( isset( $_POST['link_new_field'] ) ) {
+		// Sanitize user input.
+		$my_data = sanitize_text_field( $_POST['link_new_field'] );
+
+		// Update the meta field in the database.
+		update_post_meta( $post_id, 'ship_link', $my_data );	
+	}
+}
+
+
 
 /*
  * Place metabox above editor
