4148fa2577f1ceb09f58fa4f1bd0d125904eeacd
oceanbots <oceanbots@gmail.com>
2016-11-30 23:29:49 -0500

    No-content message
    So we can deploy without having anything actually written

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -12,15 +12,12 @@
 ?>
 
 <section class="no-results not-found">
-	<header class="page-header">
-		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'oceanbots' ); ?></h1>
-	</header><!-- .page-header -->
 
 	<div class="page-content">
 		<?php
 		if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-			<p><?php printf( wp_kses( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'oceanbots' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
+			<p><?php printf( wp_kses( __( 'I\'m uh...working on it. Articles will exist soon!', 'oceanbots' ), array( 'a' => array( 'href' => array() ) ) ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
 
 		<?php elseif ( is_search() ) : ?>
 
