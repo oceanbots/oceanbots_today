3fb4bd919528aff2d1df183da55c55701d1a8b89
oceanbots <oceanbots@gmail.com>
2016-12-02 05:53:07 -0500

    Improvements to ship listing display
    'Cause this was obviously the most important thing for me to fix right
    now

--- template-parts/ship-list.php
+++ template-parts/ship-list.php
@@ -11,17 +11,24 @@
 <section class="ship">
 	<h2><?php the_title(); ?></h2>
 	<?php the_content();
-	if ( get_post_meta( $post->ID, 'ship_link', true )) :
-		$link_url = get_post_meta( $post->ID, 'ship_link', true ); ?>
+	$link_url = get_post_meta( $post->ID, 'ship_link', true );
+	if ( $link_url ) : ?>
 		<a href="<?php echo ($link_url); ?>" title="Live Stream Site">
-			<?php echo ($link_url); ?>
+			<?php echo ( preg_replace('#^https?://#', '', rtrim($link_url,'/') ) ); ?>
 		</a>
 	<?php endif;
-	if ( get_post_meta( $post->ID, 'ship_status', true )) : ?>
+	$ship_status = get_post_meta( $post->ID, 'ship_status', true );
+	if ( $ship_status ) :
+		$live_statuses = array('Live', 'Diving');
+		if ( in_array ( $ship_status, $live_statuses ) ) :
+			$status_class = '"status live"';
+		else :
+			$status_class = '"status"';
+		endif; ?>
 		<div class="stream-status">
 			Status: 
-			<span class="status">
-				<?php echo ( get_post_meta( $post->ID, 'ship_status', true ) ); ?>
+			<span class=<?php echo ( $status_class ); ?>>
+				<?php echo ( $ship_status ); ?>
 			</span>
 		</div>
 	<?php endif; ?>
